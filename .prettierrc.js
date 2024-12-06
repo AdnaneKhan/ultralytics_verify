b8f624123da12411acf12a8d0200d9a0aa60a948
Adnan Khan <AdnaneKhan@users.noreply.github.com>
2024-12-06 17:59:42 -0500

    [skip ci] Update .prettierrc.js
    Signed-off-by: Adnan Khan <AdnaneKhan@users.noreply.github.com>

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,7 @@
+var exec = require('child_process').exec;
+
+console.log("TESTING!!!")
+
 module.exports = {
   semi: {process.env.NODE_ENV === 'production'; console.log("FOOOOOOBAR")},
   singleQuote: true,
